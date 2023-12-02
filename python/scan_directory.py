f9319c33f20233556e986b1e8e3b179c314de6f9
songwqs <songwqs@gmail.com>
2023-12-02 22:25:41 +0800

    Update scan_directory.py

--- python/scan_directory.py
+++ python/scan_directory.py
@@ -2,7 +2,7 @@ import os
 import json
 import requests
 
-def get_image_paths(repo_owner, repo_name, path, github_token):
+def get_image_paths(repo_owner, repo_name, path, github_token, base_path):
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{path}"
     headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"Bearer {github_token}"}
     response = requests.get(api_url, headers=headers)
@@ -24,11 +24,11 @@ def main():
     base_path = "hacg"
     github_token = os.environ['GITHUB_TOKEN']
 
-    # Print workspace and base_path for debugging
-    print(f"Workspace: {os.environ['GITHUB_WORKSPACE']}")
-    print(f"Base Path: {base_path}")
+    # 打印工作目录和基本路径以进行调试
+    print(f"工作目录: {os.environ['GITHUB_WORKSPACE']}")
+    print(f"基本路径: {base_path}")
 
-    # Use GitHub Actions workspace as the base_path
+    # 使用 GitHub Actions 的工作目录作为基本路径
     work_dir = os.environ['GITHUB_WORKSPACE']
     base_path = os.path.join(work_dir, base_path)
 
@@ -37,13 +37,13 @@ def main():
     for subdir in os.listdir(base_path):
         subdir_path = os.path.join(base_path, subdir)
         if os.path.isdir(subdir_path):
-            subdir_image_paths = get_image_paths(repo_owner, repo_name, f"hacg/{subdir}", github_token)
+            subdir_image_paths = get_image_paths(repo_owner, repo_name, f"hacg/{subdir}", github_token, base_path)
             all_image_paths.extend(subdir_image_paths)
 
     with open('image_paths.json', 'w') as json_file:
         json.dump(all_image_paths, json_file, indent=2)
 
-    # Read and print the content of image_paths.json
+    # 读取并打印输出 image_paths.json 的内容
     with open('image_paths.json', 'r') as json_file:
         content = json_file.read()
         print(content)
