f9dea7cdf9c365bc915b2d36dcbf55048c25134b
songwqs <songwqs@gmail.com>
2023-12-02 22:37:54 +0800

    Update scan_directory.py

--- python/scan_directory.py
+++ python/scan_directory.py
@@ -5,23 +5,27 @@ import requests
 def get_image_paths(repo_owner, repo_name, path, github_token):
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{path}"
     headers = {"Accept": "application/vnd.github.v3+json", "Authorization": f"Bearer {github_token}"}
-    response = requests.get(api_url, headers=headers)
-    response.raise_for_status()
-    contents = response.json()
+    
+    try:
+        response = requests.get(api_url, headers=headers)
+        response.raise_for_status()
+        contents = response.json()
 
-    image_paths = []
+        image_paths = []
 
-    for item in contents:
-        if item["type"] == "file" and item["name"].lower().endswith(('.jpg', '.png', '.jpeg', '.gif')):
-            full_path = f"https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{path}/{item['name']}"
-            image_paths.append(full_path)
+        for item in contents:
+            if item["type"] == "file" and item["name"].lower().endswith(('.jpg', '.png', '.jpeg', '.gif')):
+                full_path = f"https://raw.githubusercontent.com/{repo_owner}/{repo_name}/main/{path}/{item['name']}"
+                image_paths.append(full_path)
 
-    return image_paths
+        return image_paths
+    except requests.exceptions.HTTPError as e:
+        print(f"Error accessing GitHub API: {e}")
+        return []
 
 def main():
     repo_owner = "songwqs"
     repo_name = "cdnImg"
-    base_path = "hacg"
     github_token = os.environ['GITHUB_TOKEN']
 
     # Use GitHub Actions checkout path as the base_path
@@ -32,7 +36,7 @@ def main():
     for subdir in os.listdir(base_path):
         subdir_path = os.path.join(base_path, subdir)
         if os.path.isdir(subdir_path):
-            subdir_image_paths = get_image_paths(repo_owner, repo_name, f"{subdir}", github_token)
+            subdir_image_paths = get_image_paths(repo_owner, repo_name, subdir, github_token)
             all_image_paths.extend(subdir_image_paths)
 
     with open('image_paths.json', 'w') as json_file:
